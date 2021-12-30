d5cf8a2338a2f2047a7817dc67aa8260d60c0bb6
Mohamed Mousa <mohamedamousa00@gmail.com>
2021-12-30 19:25:05 +0200

    add bootstrab and theam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  //{ path: 'buildform', component: HeroesComponent },
+  { path: 'showform', component: DynamicFormComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
